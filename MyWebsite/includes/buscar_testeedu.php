81f32ebbdfb4309b83a08857908412125f9bcdf7
Marcio Barone <133541313+baronemarbles@users.noreply.github.com>
2024-10-09 18:33:59 -0300

    Add files via upload

--- MyWebsite/includes/buscar_testeedu.php
+++ MyWebsite/includes/buscar_testeedu.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf8-br">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <title>Dispositivos VTV</title>
+        <title>Buscar Dispositivos | Dispositivos VTV</title>
         <link rel="logo" href="/imgs/logo.png" type="image/png" sizes="16x16">
         <link rel="stylesheet" href="css/index.css">
         <script src="/javascript/navmenu.js"></script>
@@ -30,11 +30,18 @@
 
         <!--Criando o formulário de registro -->
 
-        <h3>Registre o dispositivo</h3>
+        <h3>É esse o seu dispositivo ?</h3>
+
+        <?php 
+        
+         require "dbh.inc.php";
+         require "search_h.php";
+        
+        ?>
 
         <!--patrimonio,numero_de_serie,marca,modelo,categoria,localizacao, status_device,observacao -->
          <div class="centralized">
-            <form action = "includes/dispositivo.inc.php" class ="form"method = "POST">
+            <form action = "includes/search_h.php" class ="form"method = "POST">
                 <input type = "text" name="patrimonio" placeholder="Patrimônio">
                 <input type ="text" name="numero_de_serie" placeholder="Número de Série">
                 <input type ="text" name="marca" placeholder="Marca">
@@ -44,7 +51,6 @@
                 <input type ="text" name="status_device" placeholder="Estado do dispositivo">
                 <input type ="text" name="observacao" placeholder="Observação">
                 <button class="buttn">Registrar</button>
-               
             </form>
         </div>
    
